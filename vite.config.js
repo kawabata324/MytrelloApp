ec57a2a83f2978f10125abf4a94b3ca2c69a10ea
kawabata <s1931115@s.konan-u.ac.jp>
2022-02-06 23:36:12 +0900

    localhostを変更

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 8000,
+  },
   base: process.env.NODE_ENV === "poduction" ? "/MytrelloApp/" : "./",
   build: {
     outDir: "docs",

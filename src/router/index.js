a95f5168558301e41bdffd66cff1f9d49f6a7cc8
kawabata <s1931115@s.konan-u.ac.jp>
2022-02-07 21:54:28 +0900

    [UPDATE]テンプレート貼り付け

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Board from "../components/Board.vue";
+import Register from "../pages/Register.vue";
 
 const routes = [
   {
     path: "/",
     name: "Board",
-    component: Board
-  }
+    component: Board,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
 ];
 
 const router = createRouter({

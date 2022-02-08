df1fd8eb175a84bd1b0b2ad6625a8d93625e36e5
kawabata <s1931115@s.konan-u.ac.jp>
2022-02-08 21:47:43 +0900

    [UPDATE]新規登録画面の作成

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,73 +1,74 @@
-<script></script>
-<!--
-  This example requires Tailwind CSS v2.0+
+<script setup>
+import { reactive } from "vue";
+import Client from "../api/client";
 
-  This example requires some changes to your config:
+const user = reactive({
+  name: "",
+  email: "",
+  password: "",
+});
 
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
+const registerUser = () => {
+  Client.post("/v1/auth", {
+    data: {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    },
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+</script>
 <template>
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-gray-50">
-    <body class="h-full">
-    ```
-  -->
-  <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+  <div
+    class="min-h-full flex flex-col items-center gap-10 py-12 px-4 sm:px-6 lg:px-8 max-h-full"
+  >
     <div class="max-w-md w-full space-y-8">
       <div>
-        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
-        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-        <p class="mt-2 text-center text-sm text-gray-600">
-          Or
-          {{ ' ' }}
-          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> start your 14-day free trial </a>
-        </p>
+        <div class="flex items-center justify-center gap-2">
+          <img
+            class="h-12 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt="Workflow"
+          />
+          <h1 class="text-2xl font-bold">Keep Making Effort</h1>
+        </div>
       </div>
-      <form class="mt-8 space-y-6" action="#" method="POST">
-        <input type="hidden" name="remember" value="true" />
-        <div class="rounded-md shadow-sm -space-y-px">
-          <div>
-            <label for="email-address" class="sr-only">Email address</label>
-            <input id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
-          </div>
-          <div>
-            <label for="password" class="sr-only">Password</label>
-            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
-          </div>
-        </div>
-
-        <div class="flex items-center justify-between">
-          <div class="flex items-center">
-            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
-            <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
-          </div>
-
-          <div class="text-sm">
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a>
-          </div>
-        </div>
-
-        <div>
-          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-              <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
-            </span>
-            Sign in
-          </button>
-        </div>
-      </form>
     </div>
+
+    <form
+      class="bg-black opacity-90 form-height w-2/3 shadow-md flex flex-col gap-10 items-center p-10 border border-gray-800"
+    >
+      <div>
+        <h3 class="text-gray-300 mt-3 text-lg">アカウントを作成</h3>
+      </div>
+      <input
+        type="text"
+        class="border shadow-lg border-gray-300 w-full px-5 py-2 text-black"
+        placeholder="名前を入力"
+        v-model="user.name"
+      />
+      <input
+        type="email"
+        class="border shadow-lg border-gray-300 w-full px-5 py-2 text-black"
+        placeholder="メールアドレスを入力"
+        v-model="user.email"
+      />
+      <input
+        type="password"
+        class="border shadow-lg border-gray-300 w-full px-5 py-2 text-black"
+        placeholder="パスワードを入力"
+        v-model="user.password"
+      />
+      <button class="bg-blue-500 w-full p-2" @submit.prevent="registerUser">
+        登録
+      </button>
+    </form>
   </div>
 </template>
+<style scoped></style>

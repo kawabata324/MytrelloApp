97adb5b3474a0d4dd0aa044fbd73d6cdc834da82
kawabata <s1931115@s.konan-u.ac.jp>
2022-02-21 10:00:49 +0900

    [UPDATE]axiosの通信をまとめるClassの作成

--- src/api/auth/register.js
+++ src/api/auth/register.js
@@ -1,1 +1,19 @@
+import Client from "../client.js";
 
+class Auth {
+  static async registerUser(user) {
+    await Client.post("/v1/auth", {
+      email: user.email,
+      password: user.password,
+      name: user.name,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+}
+
+export default Auth;
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { computed, reactive } from "vue";
-import Client from "../api/client";
 import BaseInput from "../components/common/BaseInput.vue";
+import Auth from "../api/auth/register";
 
 const user = reactive({
   email: "",
@@ -25,32 +25,23 @@ const computedUserEmail = computed({
 });
 
 const registerUser = async () => {
-  await Client.post("/v1/auth", {
-    email: user.email,
-    password: user.password,
-    name: user.name,
-  })
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-};
+  await Auth.registerUser(user);
+}
 </script>
+
 <template>
-  <div
-    class="mt-20"
-  >
-        <div class="">
-          <h1 class="text-2xl font-bold text-center">Keep Making Effort</h1>
-        </div>
+  <div class="mt-20">
+    <div class="">
+      <h1 class="text-2xl font-bold text-center">Keep Making Effort</h1>
+    </div>
 
     <form
       class="bg-black opacity-90 shadow-md w-96 p-10 border border-gray-800 rounded-md mx-auto mt-10"
     >
       <div>
-        <h3 class="text-gray-300 pb-8 text-2xl font-bold text-center">Register Your Account</h3>
+        <h3 class="text-gray-300 pb-8 text-2xl font-bold text-center">
+          Register Your Account
+        </h3>
       </div>
       <div class="flex flex-col gap-3">
         <BaseInput
@@ -78,7 +69,10 @@ const registerUser = async () => {
           v-model:value="computedUserPassword"
         />
       </div>
-      <button class="mt-8 bg-blue-900 w-full py-2 rounded-md" @click.prevent="registerUser">
+      <button
+        class="mt-8 bg-blue-900 w-full py-2 rounded-md"
+        @click.prevent="registerUser"
+      >
         Create Account
       </button>
     </form>

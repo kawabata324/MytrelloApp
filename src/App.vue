2731c15c49cf9273e55931d63443440374c4816b
kawabata <s1931115@s.konan-u.ac.jp>
2022-02-27 10:50:25 +0900

    [UPDATE]footerの完成

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
+import Footer from "./components/common/Footer.vue"
 </script>
 
 <template>
   <div id="app">
     <router-view></router-view>
     <notifications position="bottom right" />
+    <Footer class="fixed bottom-0 right-0"/>
   </div>
 </template>
 
---
+++ src/components/common/Footer.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { onMounted, reactive } from "vue";
+
+const time = reactive({
+  hours: "",
+  minutes: "",
+  seconds: "",
+});
+
+onMounted(() => {
+  setInterval(() => setTime(), 1000);
+});
+
+const setTime = () => {
+  const today = new Date();
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
+  time.hours = String(hours).padStart(2, '0')
+  time.minutes = String(minutes).padStart(2, '0')
+};
+</script>
+
+<template>
+  <div class="flex justify-end m-2">
+    <div class="flex">
+      <p>{{ time.hours }}</p>
+      <p>:</p>
+      <p>{{ time.minutes }}</p>
+    </div>
+  </div>
+</template>
